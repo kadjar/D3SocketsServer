3a27bff0d2d85cf182a1e90369c8e638f01e9752
ninjascribble <scott@ninjascript.com>
2014-04-05 13:11:16 -0700

    Reorganizing app.js in prep for SocketIO integration

--- app/app.js
+++ app/app.js
@@ -4,6 +4,8 @@ var http = require('http');
 var path = require('path');
 var exphbs = require('express3-handlebars');
 var app = express();
+var port = process.env.PORT || 3000;
+var server = http.createServer(app).listen(port);
 
 app.engine('handlebars', exphbs({
 	defaultLayout: 'main',
@@ -11,24 +13,22 @@ app.engine('handlebars', exphbs({
 	partialsDir: path.join(__dirname, 'views/partials')
 }));
 
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'handlebars');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.configure(function() {
+	app.set('port', port);
+	app.set('views', path.join(__dirname, 'views'));
+	app.set('view engine', 'handlebars');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.json());
+	app.use(express.urlencoded());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use('/static', express.static(path.join(__dirname, 'public')));
+	app.use(routes.fourohfour);
+});
 
-if ('development' == app.get('env')) {
+app.configure('development', function() {
   app.use(express.errorHandler());
-}
+});
 
 app.get('/', routes.index);
-app.use(routes.fourohfour);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
